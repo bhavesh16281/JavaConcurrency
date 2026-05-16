0758045175607428f08874ef5e91046a499d03a0
Muthyalu Bhavesh <66527078+bhavesh16281@users.noreply.github.com>
2026-05-16 21:30:54 +0530

    Initial Commit

--- src/main/java/com/bhavesh16281/app/TestThreads.java
+++ src/main/java/com/bhavesh16281/app/TestThreads.java
@@ -1,4 +1,18 @@
 package com.bhavesh16281.app;
 
+import com.bhavesh16281.app.runnables.AppThread;
+
 public class TestThreads {
+
+    public static void main(String[] args) {
+
+        AppThread  appThread1 = new AppThread();
+        AppThread  appThread2 = new AppThread();
+        AppThread  appThread3 = new AppThread();
+
+        appThread1.start();
+        appThread2.start();
+        appThread3.start();
+
+    }
 }
--- src/main/java/com/bhavesh16281/app/runnables/AppThread.java
+++ src/main/java/com/bhavesh16281/app/runnables/AppThread.java
@@ -1,4 +1,23 @@
 package com.bhavesh16281.app.runnables;
 
-public class AppThread {
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AppThread extends Thread {
+
+    @Override
+    public void run() {
+
+        try(BufferedReader reader = new BufferedReader(new FileReader("D:\\selfIntro.txt"))){
+            String line;
+
+            while((line = reader.readLine())!=null){
+
+                System.out.println(Thread.currentThread().getName()+" reading the line: "+line);
+            }
+        } catch (IOException e) {
+            Logger.getLogger(AppThread.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
 }
